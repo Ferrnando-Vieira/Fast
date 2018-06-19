10921751bc29114379d57503401fae2d8c582157
Hirazuky <nandovieira2@yahoo.com.br>
2018-06-18 23:44:26 -0300

    Adição de dados de último acesso do usuário

--- conn/logout.php
+++ conn/logout.php
@@ -1,7 +1,25 @@
 <?php
-    //Fim de sessão
+    //Inicia conexão
+    require_once 'conecta_banco.php';
 
+    //inicia sessão
     session_start();
+
+    //Id do usuario que abriu sessão
+    $id = $_SESSION['idUsuario'];
+
+    //Atualiza datahora do ultimo acesso 
+    $sql = "UPDATE usuario
+            SET ultimoAcesso = NOW()
+            WHERE idUsuario = $id;";
+
+    
+    mysqli_query($connect, $sql);
+
+    //encerra conexão
+    mysqli_close ();
+
+    //Finaliza sessão
     session_unset();
     session_destroy();
 
